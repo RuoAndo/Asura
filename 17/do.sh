f798a6d1ae04cd855d5ce03bd2f1f4c449bbba97
Ruo Ando <ando.ruo@gmail.com>
2019-03-31 16:36:27 +0900

    auto

--- 17/do.sh
+++ 17/do.sh
@@ -1,6 +1,10 @@
 ./build-asura.sh asura
 ./build-gpu.sh reduce
+./build-gpu.sh km-thrust
 
 time ./asura $1
 time ./reduce.sh tmp-asura-1
 time ./reduce.sh tmp-asura-2
+
+nLines=`wc -l tmp-asura-1-reduced`
+time ./km-thrust tmp-asura-1-reduced tmp-asura-2-reduced $nLines
