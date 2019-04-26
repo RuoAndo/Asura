4984630ce0cfb095a56f3cd367498eec9602271a
Ruo Ando <ando.ruo@gmail.com>
2019-04-26 14:28:33 +0900

    Update kmeans_tbb.h

--- 1/kmeans_tbb.h
+++ 1/kmeans_tbb.h
@@ -2,8 +2,7 @@ void compute_k_means( size_t n, const point points[], size_t k, cluster_id id[],
 
     tls_type tls([&]{return k;}); 
     view global(k);
-
-    // Create initial clusters and compute their sums.
+    
     tbb::parallel_for(
         tbb::blocked_range<size_t>(0,n),
         [=,&tls,&global]( tbb::blocked_range<size_t> r ) {
@@ -15,41 +14,32 @@ void compute_k_means( size_t n, const point points[], size_t k, cluster_id id[],
             }
         }
     );
-
-    // Loop until ids do not change
+    
     size_t change;
     do {
-        // Reduce local sums to global sum
-        reduce_local_sums_to_global_sum( k, tls, global );
-
-        // Repair any empty clusters
+        convert_local_sums_to_global_sum( k, tls, global );
         repair_empty_clusters( n, points, id, k, centroid, global.array );
-
-        // "Divide step": Compute centroids from global sums
+        
         for( size_t j=0; j<k; ++j ) {
             centroid[j] = global.array[j].mean();
             global.array[j].clear();
         }
 
-        // Compute new clusters and their local sums
         tbb::parallel_for(
             tbb::blocked_range<size_t>(0,n),
             [=,&tls,&global]( tbb::blocked_range<size_t> r ) {
                 view& v = tls.local();
                 for( size_t i=r.begin(); i!=r.end(); ++i ) {
-                    // "Reassign step": Find index of centroid closest to points[i]
                     cluster_id j = calc_shortest_index(centroid, k , points[i]); 
                     if( j!=id[i] ) {
                         id[i] = j;
                         ++v.change;
                     }
-                    // "Sum step" 
                     v.array[j].tally(points[i]);
                 }
             }
         );
-
-        // Reduce local counts to global count
-        reduce_local_counts_to_global_count( tls, global );
+        
+        convert_local_counts_to_global_count( tls, global );
     } while( global.change!=0 );
 }
