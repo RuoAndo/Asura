7a87f79cb205fdaa2264dd9bc728f972400a2a72
Ruo Ando <ando.ruo@gmail.com>
2019-04-29 13:48:33 +0900

    asura

--- 22/build-asura.sh
+++ 22/build-asura.sh
@@ -5,4 +5,4 @@
 
 nvcc -g -c $1.cpp -std=c++11 -ltbb -Xcompiler="-fpermissive, -lpthread"
 nvcc -g -c kernel.cu
-nvcc -g $1.o kernel.o -ltbb --generate-code arch=compute_60,code=sm_60
+nvcc -g -o $1 $1.o kernel.o -ltbb --generate-code arch=compute_60,code=sm_60
