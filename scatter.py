d89de6e0db3ce2f03eb8edc9f1e7d32c39578f34
Ruo Ando <ando.ruo@gmail.com>
2021-03-17 21:17:50 +0900

    scatter

--- scatter.py
+++ scatter.py
@@ -7,15 +7,12 @@ data_set = np.loadtxt(
     delimiter=",",
 )
 
-#散布図を描画 → scatterを使用する
-#1行ずつ取り出して描画
-#plt.scatter(x座標の値, y座標の値)
 for data in data_set:
     plt.scatter(data[0], data[1])
 
-plt.title("correlation")
-plt.xlabel("Average Temperature of SAITAMA")
-plt.ylabel("Average Temperature of IWATE")
+plt.title("packets")
+plt.xlabel("counts")
+plt.ylabel("bytes")
 plt.grid()
 
 plt.show()
